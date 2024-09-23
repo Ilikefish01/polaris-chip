fad2e6fdc3d9d23e343fc1d829133d154977f449
Krittanat Kulsakdinun <79750745+Ilikefish01@users.noreply.github.com>
2024-09-22 22:17:39 -0400

    Update my-card.js

--- src/my-card.js
+++ src/my-card.js
@@ -15,23 +15,73 @@ export class MyCard extends LitElement {
   constructor() {
     super();
     this.title = "My card";
+    this.image = "Image";
+    this.description = "Description";
+    this.borderColor = "white";
   }
 
   static get styles() {
     return css`
       :host {
+        display: inline-flex;
+      }
+
+      span {
+        background-color: blue;
+        color: black;
+        font-size: 24px;
+        padding: 16px;
+        margin: 8px;
+      }
+
+      span:hover {
+        background-color: grey;
+        border: 1px solid black;
+      }
+
+      .card{
+        margin: 0 auto;
+        max-width: 300px;
+        max-height: 400px;
+        padding: 10px;
+        border-radius: 5px;
+        background-color: aqua;
+      }
+
+      .small{
+        font-size: 12px;
+        text-align: center;
+      }
+
+      .button{
+        margin: auto;
         display: block;
       }
     `;
   }
 
   render() {
-    return html`<div>${this.title}</div>`;
+    return html`
+    <div class="cardList">
+      <div class="card">
+        <img class="image">${this.src}</img>
+        <h2 class="title">${this.title}</h2>
+        <p class="description">${this.description}</p>
+        <p class="link">${this.link}</p>
+      </div>
+    </div>
+
+    `;
   }
+  
 
   static get properties() {
     return {
       title: { type: String },
+      image: {type: String},
+      description: {type: String},
+      link: {type: String},
+      borderColor: {type: String},
     };
   }
 }
